7b3c9755180f6881ac35d5ec2a5369535b2844bb
Bhavani1615 <devulapallibhavani1509@gmail.com>
2020-12-09 10:08:21 +0530

    sprint2

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -1,21 +1,21 @@
-package Runners;
-
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-features = "E:\\javaselenium\\DemoWebShopMaven\\src\\test\\java\\features"  // path of feature file
-,glue={"StepDefinitions"}  //path of step definition class
-,tags={"@SuccessfulLogin"}  //to run exactly the required scenario
-,plugin = {"pretty", "html:target/cucumber-html-report"}
-,monochrome=true// console output is much more readable
-
-
-)
-public class TestRunner extends AbstractTestNGCucumberTests {
-
-}
+//package Runners;
+//
+//import org.junit.runner.RunWith;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//import cucumber.api.testng.AbstractTestNGCucumberTests;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//features = "E:\\javaselenium\\DemoWebShopMaven\\src\\test\\java\\features"  // path of feature file
+//,glue={"StepDefinitions"}  //path of step definition class
+//,tags={"@SuccessfulLogin"}  //to run exactly the required scenario
+//,plugin = {"json:target/cucumber.json"}
+//,monochrome=true// console output is much more readable
+//
+//
+//)
+//public class TestRunner extends AbstractTestNGCucumberTests {
+//
+//}
--- src/test/java/Runners/TestRunnerTestng.java
+++ src/test/java/Runners/TestRunnerTestng.java
@@ -6,7 +6,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 features = "E:\\javaselenium\\DemoWebShopMaven\\src\\test\\java\\features" // path of feature file
 ,glue={"StepDefinitions"}  //path of step definition class
-//,tags={"@SuccessfulLogin"} 
+,tags={"@SuccessfulLogin"} 
 
 ,monochrome=true// console output is much more readable
 ,plugin = {"json:target/cucumber.json"}
